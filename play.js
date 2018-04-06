e5541b5f43c865512b1f588c516d139e6fef0f16
marco79cgn <marco79cgn@gmail.com>
2018-04-06 18:33:47 +0200

    revert changes for preloading

--- play.js
+++ play.js
@@ -2,7 +2,7 @@ var flacApiBaseUrl = 'https://api.radioparadise.com/api/get_block?bitrate=4&info
 var flacApiNextEventUrl = 'https://api.radioparadise.com/api/get_block?bitrate=4&info=true';
 var nextStream;
 
-function getFirstEvent(callback) {	
+function getNextEvent(callback) {	
 	const xhr = new XMLHttpRequest();
 	xhr.open('get', 'https://crossorigin.me/'+flacApiNextEventUrl, true);
     xhr.onload = function(e) {
@@ -15,33 +15,18 @@ function getFirstEvent(callback) {
     xhr.send();
 }
 
-function prepareNextEvent() {	
-	const xhr = new XMLHttpRequest();
-	xhr.open('get', 'https://crossorigin.me/'+flacApiNextEventUrl, true);
-    xhr.onload = function(e) {
-  		var data = JSON.parse(this.response);
-  		console.log(data);
-  		nextStream = data.url+'?src=alexa';
-  		flacApiNextEventUrl = flacApiBaseUrl + '&event=' + data.end_event;
-	}
-    xhr.send();
-}
-
 function playStream() {
 	console.log('Playing Stream ' + nextStream);
 	var stream = new Howl({
     	src: [nextStream],
 		ext: ['flac'],
     	autoplay: true,
-		html5: true,
-		onplay: function() {
-			prepareNextEvent();			
-		},
+    	html5: true,
     	onend: function() {
-    		playStream();
+    		getNextEvent(playStream);
   		}
 	});
 	stream.play();
 }
 
-getFirstEvent(playStream);
+getNextEvent(playStream);
