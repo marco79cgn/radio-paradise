02c32580c6a79c7359fd89d5803ddbf62a30af0b
marco79cgn <marco79cgn@gmail.com>
2018-04-09 12:12:56 +0200

    fixed format

--- play.js
+++ play.js
@@ -5,26 +5,26 @@ var nextStream;
 function getNextEvent(callback) {	
 	const xhr = new XMLHttpRequest();
 	xhr.open('get', 'https://crossorigin.me/'+flacApiNextEventUrl, true);
-    xhr.onload = function(e) {
-  		var data = JSON.parse(this.response);
-  		console.log(data);
+	xhr.onload = function(e) {
+		var data = JSON.parse(this.response);
+		console.log(data);
   		nextStream = data.url+'?src=alexa';
   		flacApiNextEventUrl = flacApiBaseUrl + '&event=' + data.end_event;
   		callback();
 	}
-    xhr.send();
+	xhr.send();
 }
 
 function playStream() {
 	console.log('Playing Stream ' + nextStream);
 	var stream = new Howl({
-    	src: [nextStream],
+		src: [nextStream],
 		ext: ['flac'],
-    	autoplay: true,
-    	html5: true,
-    	onend: function() {
-    		getNextEvent(playStream);
-  		}
+		autoplay: true,
+		html5: true,
+		onend: function() {
+			getNextEvent(playStream);
+		}
 	});
 	stream.play();
 }
