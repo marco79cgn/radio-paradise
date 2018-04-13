ef1bb04a063b90f4f4eaaf2969cb16294ee6378c
Marco Dengel <marco79cgn@gmail.com>
2018-04-13 08:54:46 +0200

    added autoplay after loading

--- player.js
+++ player.js
@@ -292,6 +292,7 @@ var nextPlaylist;
 // Setup our new audio player class and pass it the playlist.
 var firstEvent = getNextEvent(false);
 var player = new Player(buildPlaylistForFirstEvent(firstEvent));
+player.play();
 
 function getNextEvent(isNotBlocking, self, callback) {	
 	const xhr = new XMLHttpRequest();
